0f58c5896562472547c56d058c7d7e87caa53726
Heather <2201826@uad.ac.uk>
2025-01-22 16:24:00 +0000

    Second test push! Very rough timer code included

---
+++ Bean-Game/Assets/Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float waitingTime;
+    private float timeRemaining = 0;
+    public bool timerRestart;
+
+    private void Start()
+    {
+        timerRestart = false;
+        timeRemaining = waitingTime;
+    }
+
+    void Update()
+    {
+
+        //Debug.Log("script is running");
+
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+           // Debug.Log(timeRemaining);
+        }  
+
+        else
+        {
+            Debug.Log("Customer has run out of patience!");
+            timerRestart = true;
+        }
+
+        if (timerRestart)
+        { 
+            timeRemaining = waitingTime;
+            timerRestart = false;
+        }
+    }
+}
