503d1fd0a2b2e1813043565a21777f8772503f79
Heather <2201826@uad.ac.uk>
2025-01-28 14:08:58 +0000

    Customer Variation Added + Tipping Calculation

--- Bean-Game/Assets/Customer/IncomeSystem.cs
+++ Bean-Game/Assets/Customer/IncomeSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,19 +12,21 @@ public class IncomeSystem : MonoBehaviour
     {
         income = 0;
     }
+
     public void IncreaseIncome(float amount)
     {
         income += amount;
+        income = Mathf.Round(income * 100.0f) * 0.01f; // Round income every update
     }
 
     public void DecreaseIncome(float amount)
     {
         income -= amount;
+        income = Mathf.Round(income * 100.0f) * 0.01f; // Round income every update
     }
 
     public float GetIncome()
     {
         return income;
     }
-
 }
