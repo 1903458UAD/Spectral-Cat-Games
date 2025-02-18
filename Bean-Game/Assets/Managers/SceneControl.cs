434ce83ef80e449c57cc9e4ec8fcc1c1f7ef4c59
andyandme <119869730+andyandme@users.noreply.github.com>
2025-02-18 16:30:43 +0000

    Added a Pause menu
    ToDO:
    -No cursor to click on the buttons
    -Need to make further menu for settings
    -Need to stop camera from moving when paused

--- Bean-Game/Assets/Managers/SceneControl.cs
+++ Bean-Game/Assets/Managers/SceneControl.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 
 public class SceneControl : MonoBehaviour
 {
+
+    [SerializeField] private GameObject PauseMenuUI;
+    private bool isPaused = false;
+
     private void Start()
     {
         GameManager.Instance.SetIncome(StaticData.incomePassed);
         UIManager.Instance.UpdateIncomeDisplay(GameManager.Instance.GetIncome());
+
+        if (PauseMenuUI != null)
+        {
+            PauseMenuUI.SetActive(false);
+        }
     }
 
     private void Update()
@@ -18,6 +28,51 @@ public class SceneControl : MonoBehaviour
         {
             GameManager.Instance.ChangeScene(1);
         }
+
+        //Toggle Pause: (Currently Escape key)
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+
+    private void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f; // Pause the game
+            PauseMenuUI.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f; // Resume the game
+            PauseMenuUI.SetActive(false);
+        }
+    }
+
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        PauseMenuUI.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        //SceneManager.LoadScene(0); 
+    }
+    public void Settings()
+    {
+    //Send to another menu
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
 
