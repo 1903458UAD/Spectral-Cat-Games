5289894a6e80d2a85ec09f31babde4c0d36caa1e
1903458UAD <1903458@uad.ac.uk>
2025-03-13 14:04:16 +0000

    Pre-build updates
    Added materials to props, and toggle lights.
    
    Added collision to all walls

--- Bean-Game/Assets/Player Scripts/PlayerInteraction.cs
+++ Bean-Game/Assets/Player Scripts/PlayerInteraction.cs
@@ -12,6 +12,7 @@ public class PlayerInteraction : MonoBehaviour
     public LayerMask InteractableObjectLayer;
     public LayerMask FunctionalObjectLayer;
     public Transform cameraTransform;
+    public GameObject lightContainer;
 
     private InteractableObject heldObjectRight; // Right-hand object
     private InteractableObject heldObjectLeft;  // Left-hand object
@@ -32,6 +33,7 @@ public class PlayerInteraction : MonoBehaviour
 
         Pickup_AND_Interact = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("InteractKey", "Mouse0")); //Player pref saves over game sessions, It is also a new concept for me, Documentation: https://docs.unity3d.com/6000.0/Documentation/ScriptReference/PlayerPrefs.html
         Drop = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("DropKey", "Mouse1"));
+        lightContainer = GameObject.Find("LightsContainer");
     }
 
     public void UpdateKeybindings()
@@ -41,8 +43,24 @@ public class PlayerInteraction : MonoBehaviour
         Drop = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("DropKey", "Mouse1"));
     }
 
+    public void toggleLights()
+    {
+        if(lightContainer.activeSelf)
+        {
+            lightContainer.SetActive(false);
+        }
+        else
+        {
+            lightContainer.SetActive(true);
+        }
+    }
+
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            toggleLights();
+        }
 
         if (cameraTransform == null)
         {
