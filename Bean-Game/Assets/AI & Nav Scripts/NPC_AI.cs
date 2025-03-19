227737aa84c05d0c64d1b4f1a6e1c2a74b08efcc
2201236 <2201236@uad.ac.uk>
2025-03-19 15:55:38 +0000

    Added new beans and their movement  animation
    Added the new bean prefab with it's movement animation. Works so that the beans only bounce when on the move and stop bouncing when in hiding spots. Fixed bug of not being able to pickup bean after dropping(Issue was in InteractableObjects cs. Implemented stop for sound when bean is picked up, NPC_AI cs was being set to inactive in the InteractableObjects cs which was preventing the code from updating when the bean was picked up.

--- Bean-Game/Assets/AI & Nav Scripts/NPC_AI.cs
+++ Bean-Game/Assets/AI & Nav Scripts/NPC_AI.cs
@@ -40,7 +40,16 @@ public class NPC_AI : MonoBehaviour
     public bool hasReachedRouteEnd = false;
 
 
+    public AnimatorStateInfo stateInfo;
 
+    public Animator animator;
+
+    public AnimationClip loopingClip;
+    public AnimationClip nonLoopingClip;
+
+
+    public string looping = "Take 001";
+    public string nonLooping = "Take 002";
 
     public enum NPCState { Idle, Hiding, Running }
     public NPCState state = NPCState.Idle;
@@ -53,6 +62,7 @@ public class NPC_AI : MonoBehaviour
     private void Start()
     {
         beanFootsteps = AudioManager.instance.CreateInstance(FMODEvents.instance.beanFootsteps);
+        animator = GetComponent<Animator>();
     }
 
     private void Awake()
@@ -63,7 +73,11 @@ public class NPC_AI : MonoBehaviour
 
     private void Update()
     {
-        if (isPickedUp) return;
+        if (isPickedUp)
+        {
+            beanFootsteps.stop(STOP_MODE.IMMEDIATE);
+            return;
+        }
 
         if (navMeshAgent == null)
         {
@@ -71,6 +85,8 @@ public class NPC_AI : MonoBehaviour
         }
 
         distanceToSpot = Vector3.Distance(transform.position, GetHidingSpotPosition());
+
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
     }
 
     public void MoveTo(Vector3 destination)
@@ -173,7 +189,7 @@ public class NPC_AI : MonoBehaviour
         }
         else 
         {
-            beanFootsteps.stop(STOP_MODE.ALLOWFADEOUT);
+            beanFootsteps.stop(STOP_MODE.IMMEDIATE);
         }
     }
 
@@ -185,7 +201,7 @@ public class NPC_AI : MonoBehaviour
         {
             currentHidingSpot.DecrementOccupancy();  //-1 from spot if the bean was hiding
             Debug.Log($"[NPC_AI] {gameObject.name} was picked up and left hiding spot {currentHidingSpot.name}");
-            currentHidingSpot = null; // Remove reference to the hiding spot
+            SetHidingSpot(null); // Remove reference to the hiding spot
         }
 
         if (navMeshAgent != null)
@@ -198,6 +214,9 @@ public class NPC_AI : MonoBehaviour
     public void OnDropped()
     {
         isPickedUp = false;
+
+
+
         if (navMeshAgent == null)
         {
             return;
@@ -214,9 +233,11 @@ public class NPC_AI : MonoBehaviour
         }
         else
         {
-            Debug.LogError($"[NPC_AI] {gameObject.name} could not find a valid NavMesh position nearby! Disabling movement.");
-            navMeshAgent.enabled = false; // Prevent errors if no valid NavMesh position
+            //Debug.LogError($"[NPC_AI] {gameObject.name} could not find a valid NavMesh position nearby! Disabling movement.");
+            //navMeshAgent.enabled = false; // Prevent errors if no valid NavMesh position
         }
+
+        state = NPCState.Idle;
     }
 
     public bool IsPickedUp()
