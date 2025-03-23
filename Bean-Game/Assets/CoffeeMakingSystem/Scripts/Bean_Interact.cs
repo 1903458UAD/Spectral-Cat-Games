94c4ec086dcc3219258b250d87e0c172e3d4828e
2201236 <2201236@uad.ac.uk>
2025-03-23 19:05:05 +0000

    Added cage functionality
    Added cage functionality, bean is dropped into cage which is set as a hiding spot type that the bean cannot exit. Needs testing but so far looks like there should be no issue with implementation.

--- Bean-Game/Assets/CoffeeMakingSystem/Scripts/Bean_Interact.cs
+++ Bean-Game/Assets/CoffeeMakingSystem/Scripts/Bean_Interact.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
+using Debug = UnityEngine.Debug;
 
 public class BeanInteraction : MonoBehaviour
 {
+    private Hiding_Spots cage;
+    private List<NPC_AI> npc = new List<NPC_AI>();
+
     //Beans are only added if held in the player's hand, Removed the Colliding Code
     public void TryAddToCoffeeMachine(CoffeeMachine coffeeMachine)
     {
@@ -17,6 +22,29 @@ public class BeanInteraction : MonoBehaviour
             
         }
        
+    }
+
+    public void TryAddToCage(Hiding_Spots cage)
+    {
+        if (cage != null)
+        {
+            InteractableObject interactable = GetComponent<InteractableObject>();
+            NPC_AI beanNPC = GetComponent<NPC_AI>();
+
+
+            if (interactable != null && interactable.GetIsHeld())
+            {
+                interactable.ReleaseObject();
+            }
+
+            if (beanNPC != null)
+            {
+                beanNPC.SetHidingSpot(cage);
+            }
+
+            cage.AddBean(this);
+        }
+
     }
 
 }
--- Bean-Game/Assets/Managers/StaticData.cs
+++ Bean-Game/Assets/Managers/StaticData.cs
@@ -10,7 +10,9 @@ public class StaticData : MonoBehaviour
     public static float tipAmount = 50;
     public static float customerPatience = 1;
     public static float longArm = 2;
-    public static float alertSize = 0.5;
+    public static float alertSize = 1;
+    public static float cagePurchased = 0;
+    public static float trapPurchased = 0;
 
     public static int lowerQuotaLimit = 3;
     public static int higherQuotaLimit = 5;
@@ -18,7 +20,7 @@ public class StaticData : MonoBehaviour
 
     public static bool dualWieldUpgrade = false;
     public static bool alert = false;
-    public static bool trapPurchased = false;
-    public static bool cagePurchased = false;
+    
+
     public static bool inflateBean = false;
 }
