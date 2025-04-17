efee1b3227309f30b1d1a5cae9db5a25682752a1
1903458UAD <1903458@uad.ac.uk>
2025-04-17 11:45:24 +0100

    Added car movement
    Still needs to have correct rotation

---
+++ Bean-Game/Assets/Passing Cars/CarBehaviour.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour
+{
+    private Transform node1;
+    private Transform node2;
+    private Transform node3;
+
+    public Transform currentNode;
+    public float carSpeed;
+
+    public PassingCarsScript PCS;
+
+    private void Start()
+    {
+        PCS = gameObject.GetComponentInParent<PassingCarsScript>();
+
+        node1 = PCS.node1;
+        node2 = PCS.node2;
+        node3 = PCS.node3;
+        carSpeed = .5f;
+
+        currentNode = node1;
+    }
+
+    private void Update()
+    {
+        if (currentNode == node1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, node2.position, carSpeed);
+
+            if (Vector3.Distance(transform.position, node2.position) < 0.001f)
+            {
+                currentNode = node2;
+            }
+        }
+        else if (currentNode == node2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, node3.position, carSpeed);
+
+            if (Vector3.Distance(transform.position, node3.position) < 0.001f)
+            {
+                currentNode = node3;
+            }
+        }
+        else if (currentNode == node3)
+        {
+            PCS.DespawnCar(gameObject);
+        }
+    }
+}
