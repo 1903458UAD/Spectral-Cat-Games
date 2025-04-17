0f18bc3c14d8568b88aeaba5b103ad4caff6fd86
andyandme <119869730+andyandme@users.noreply.github.com>
2025-04-17 23:34:59 +0100

    Syrups not active on day 1

--- Bean-Game/Assets/Art Assets/Textures/Customer/Scripts/CustomerScript.cs
+++ Bean-Game/Assets/Art Assets/Textures/Customer/Scripts/CustomerScript.cs
@@ -93,10 +93,19 @@ public class CustomerScript : MonoBehaviour
         requiredBeans = UnityEngine.Random.Range(1, 4);
 
         // Debug.Log($"Customer wants a coffee with {requiredBeans} beans.");
+        
+        int currentDay = DayManager.Instance != null ? DayManager.Instance.currentDay : 1;
 
+        if (currentDay == 1)
+        {
+            requiredSyrup = "None";
+        }
+        else
+        {
+            string[] syrups = { "Peanut Butter", "Vanilla", "Iced Tea", "Caramel", "None" };
+            requiredSyrup = syrups[UnityEngine.Random.Range(0, syrups.Length)];
+        }
 
-        string[] syrups = { "Peanut Butter", "Vanilla", "Iced Tea", "Caramel", "None" };
-        requiredSyrup = syrups[UnityEngine.Random.Range(0, syrups.Length)];
         UIManager.Instance.UpdateCustomerOrder(requiredBeans, requiredSyrup);
     }
 
